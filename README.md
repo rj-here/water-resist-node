ff376ca47ce4a8cf287ecfd1f96a6e932edfe882
rj-here <r.rishi.rj@gmail.com>
2026-06-26 16:51:52 -0400

    Getting started
    Set up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # water-resist-node
 A simple project about water resistance. Extra goal: use Node.js
+Last updated: 26th June, 2026
---
+++ app.js
@@ -0,0 +1,19 @@
+function intro() {
+    console.log("Welcome! This is a simple app about water resistance, a concept that intrigues me a little bit. The other objective: getting familiar with Node.js. I hope you enjoy it!");
+}
+
+function main() {
+    console.log("Let's get started!");
+    console.log("This app will help you understand water resistance in a fun and interactive way.");
+    console.log("What would you like to learn? Type a number based on what comes next:");
+    options();
+}
+
+function options() {
+    console.log("Please choose an option:");
+    console.log("0: EXIT!")
+    console.log("1: Learn about water resistance");
+    console.log("2: Learn about the code (IP67, for example)");
+    
+
+}
