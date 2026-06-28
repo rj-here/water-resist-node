724643550a7e1097f5863718076b9c6b1bd928d6
rj-here <r.rishi.rj@gmail.com>
2026-06-28 11:07:52 -0400

    Work

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function intro() {
     console.log("Welcome! This is a simple app about water resistance, a concept that intrigues me a little bit. The other objective: getting familiar with Node.js. I hope you enjoy it!");
+    main();
 }
 
 function main() {
@@ -12,8 +13,18 @@ function main() {
 function options() {
     console.log("Please choose an option:");
     console.log("0: EXIT!")
-    console.log("1: Learn about water resistance");
-    console.log("2: Learn about the code (IP67, for example)");
-    
+    console.log("1: Learn about IP code basics");
+    console.log("2: Meaning of each digit in the IP code");
+    console.log("3: Examples of IP codes");
+}
 
+function exit() {
+    console.log("Thank you for using the app! Goodbye!");
+    process.exit(0);
+}
+
+function learnIPBasics() {
+    console.log("IP code, or Ingress Protection code is a standard to indicate device protection against water and dust.")
+    console.log("defined by International Electrotechnical Commission under IEC 60529.");
+    console.log("The digits indicate conformity with conditions summarized. Also, no hyphens!");
 }
